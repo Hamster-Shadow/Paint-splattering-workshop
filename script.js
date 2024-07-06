a814352cd25619c41cb188178214b1116e768b61
Hamster-Shadow <174282494+Hamster-Shadow@users.noreply.github.com>
2024-07-06 19:42:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function onMouseMove(event) {
+  var path = new Path.Circle({
+    center: event.middlePoint,
+    radius: event.delta.length
+  })
+  path.fillColor = {
+    hue: event.count * 56,
+    saturation: 1,
+    brightness: 1
+  }
+}
